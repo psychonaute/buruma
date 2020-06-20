e313f3079dd3f5eb714a37aa29da563a9d8ea099
Killian Blais <killian.blais@gmail.com>
2020-06-20 09:49:20 +0700

    - bundling css/jss with browserify and uglify-js not working

---
+++ node_main/js/main.js
@@ -0,0 +1,4 @@
+// ---- main.js -----
+module.exports.Carousel = require('../../node_modules/bulma-carousel/dist/js/bulma-carousel.js');
+module.exports.Accordion = require('../../node_modules/bulma-accordion/dist/js/bulma-accordion.js');
+module.exports.Menu = require('./menu.js');
